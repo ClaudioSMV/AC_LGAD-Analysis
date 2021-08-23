e24635f48a6d6c7a68989cdf28584323ce9a39fb
Claudio San Martin <claudio.sanmartinval@gmail.com>
2021-08-23 17:17:11 -0400

    improve format and add amp 1dim hists

--- ampMacro.C
+++ ampMacro.C
@@ -11,11 +11,12 @@ void ampMacro(TString volt = "200"){
         return -1;
     }
 
-    // TString txt = "/user/s/sanmartin/work/AC_LGAD/run-files/BNL2020_"+volt+"V_"+num+"_rootFileList.txt";
-    TString txt = "/home/claudio/work/ac-lgad/AC_LGAD-data/list_runs.txt";
+    TString txt = "../run-files/BNL2020_"+volt+"V_"+num+"_rootFileList.txt";
     std::ifstream ifile(txt);
 
     TChain *chain = new TChain("pulse");
+
+    TFile *output = TFile::Open("BNL2020_"+volt+".root","RECREATE");
 
     int counter = 0;
     if (ifile.is_open()){
@@ -25,7 +26,7 @@ void ampMacro(TString volt = "200"){
 
             if (ifile.eof()) break;
 
-            chain->Add("/home/claudio/work/ac-lgad/AC_LGAD-data/"+line);
+            chain->Add("../AC_LGAD-data/"+line);
             counter++;
         }
         ifile.close();
@@ -38,21 +39,29 @@ void ampMacro(TString volt = "200"){
     std::cout << "Total number of files read: " << counter << std::endl;
     std::cout << "Total number of entries: " << Nentries << std::endl;
 
-    // TString amp_low_cut[7] = {"15", "12", "11", "12", "12", "10", "11"};
     int amp_low_cut[7] = {15, 12, 11, 12, 12, 10, 11};
+    float xy_position[4] = {-1.5, 1.5, 9.5, 12.5};
 
     for (int channel=0; channel<7; channel++){
-        // std::string channel_name = Form("%i",channel);
-        TCut xCut = "x_dut[0]>-1 && x_dut[0]<1";
-        TCut yCut = "y_dut[0]>9.5 && y_dut[0]<12.5";
+        auto htitleamp = Form("amp[%i];amp[%i] [mV];Counts",channel,channel);
+        TH1F *histamp = new TH1F(Form("amp%i",channel),htitleamp,100,0,50);
+        chain->Draw(Form("amp[%i]>>amp%i",channel,channel),Form("amp[%i]>0 && amp[%i]<100",channel,channel));
+
+        // float cont = histamp->GetBinContent(histamp->FindBin(amp_low_cut[channel]));
+        // TLine *line = new TLine(amp_low_cut[channel],0,amp_low_cut[channel],cont);
+        // line->SetLineColor(kRed);
+        // line->Draw();
+
+        TCut xCut = Form("x_dut[0]>%.1f && x_dut[0]<%.1f",xy_position[0],xy_position[1]);
+        TCut yCut = Form("y_dut[0]>%.1f && y_dut[0]<%.1f",xy_position[2],xy_position[3]);
         TCut ampCut = Form("amp[%i]>%i",channel,amp_low_cut[channel]);
 
-        auto htitleamp = Form("amp[%i];amp[%i] [mV];Counts",channel,channel);
-        TH1F *histamp = new TH1F(Form("amp%i",channel),htitleamp,100,0,400);
-        auto htitlexy = Form("Hits in channel %i with amp[%i]>%i;x_dut [mm];y_dut [mm]",channel,channel,amp_low_cut[channel]);
-        TH2F *histxy = new TH2F(Form("ampxy%i",channel),htitlexy,100,-1,1,10,9.5,12.5);
-
-        chain->Draw(Form("amp[%i]>>amp%i",channel,channel),Form("amp[%i]>0",channel));
+        auto htitlexy = Form("Hits in channel %i with amp[%i]>%i;x_dut [cm];y_dut [cm]",channel,channel,amp_low_cut[channel]);
+        TH2F *histxy = new TH2F(Form("ampxy%i",channel),htitlexy,100,xy_position[0],xy_position[1],
+                                10,xy_position[2],xy_position[3]);
         chain->Draw(Form("y_dut[0]:x_dut[0]>>ampxy%i",channel),xCut+yCut+ampCut,"COLZ");
     }
+
+    output->Write();
+    output->Close();
 }
