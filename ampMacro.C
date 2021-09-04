1f7bace8fac0cfabcb6cda425a4ec27a94269e31
Claudio San Martin <claudio.sanmartinval@gmail.com>
2021-09-03 22:30:16 -0400

    add amp plot for channels 1-6 with spatial cuts + add support for Photek (last entry [7])

--- ampMacro.C
+++ ampMacro.C
@@ -43,25 +43,33 @@ void ampMacro(TString volt = "200", bool local = false){
     std::cout << "Total number of entries: " << Nentries << std::endl;
 
     // Amp threshold value and position of wires 
-    int amp_low_cut[7] = {60, 60, 60, 70, 70, 70, 70};
+    int amp_low_cut[8] = {60, 60, 60, 70, 70, 70, 70, 70};
     float xy_position[4] = {-1.1, 1.5, 9.5, 12.2}; // {x_min, x_max, y_min, y_max}
     float ch_limits[6][4] = {{0.28, 0.43, 10.0, 11.6}, {0.18, 0.30, 10, 11.6}, {0.09, 0.22, 10, 11.6},
                             {-0.01, 0.12, 10.0, 11.6}, {-0.11, 0.01, 10, 11.6}, {-0.21, -0.09, 10, 11.6}};
 
-    TH1F *hamp_Tot = new TH1F("amp_Tot","amp for all channels;amp [mV];Counts",400,0,400);
+    TH1F *hamp_Tot = new TH1F("amp_Tot","amp for channels 1-6;amp [mV];Counts",400,0,400);
+    TH1F *hamp_Tot_chcut = new TH1F("amp_Tot_chcut","amp for channels 1-6, with spatial cut;amp [mV];Counts",400,0,400);
     // TH2F *histxy_st0 = new TH2F("histxy_st0","Stack of channels 0 to 6",100,xy_position[0],xy_position[1],
     //                            10,xy_position[2],xy_position[3]);
     TH2F *histxy_st1 = new TH2F("histxy_st1","Stack of channels 1 to 6",100,xy_position[0],xy_position[1],
                                 10,xy_position[2],xy_position[3]);
 
-    for (int channel=0; channel<7; channel++){
+    for (int channel=0; channel<8; channel++){
         // Simple amp plot
         auto htitleamp = Form("amp[%i];amp[%i] [mV];Counts",channel,channel);
         TH1F *histamp = new TH1F(Form("amp%i",channel),htitleamp,400,0,400);
         chain->Draw(Form("amp[%i]>>amp%i",channel,channel),Form("amp[%i]>0",channel)); // && amp[%i]<100
-        if (channel!=0) hamp_Tot->Add(histamp);
+        if (channel!=0 && channel!=7) hamp_Tot->Add(histamp);
 
         // x_dut vs y_dut
+        if (channel==7){
+            xy_position[0] = -1.8;
+            xy_position[1] = 1.8;
+            xy_position[2] = 9.0;
+            xy_position[3] = 13.0;
+        }
+
         TCut xCut = Form("x_dut[0]>%.1f && x_dut[0]<%.1f",xy_position[0],xy_position[1]);
         TCut yCut = Form("y_dut[0]>%.1f && y_dut[0]<%.1f",xy_position[2],xy_position[3]);
         TCut ampCut = Form("amp[%i]>%i",channel,amp_low_cut[channel]);
@@ -71,34 +79,33 @@ void ampMacro(TString volt = "200", bool local = false){
                                 10,xy_position[2],xy_position[3]);
         chain->Draw(Form("y_dut[0]:x_dut[0]>>ampxy%i",channel),xCut+yCut+ampCut,"COLZ");
         // histxy_st0->Add(histxy);
-        if (channel!=0) histxy_st1->Add(histxy);
+        if (channel!=0 && channel!=7) histxy_st1->Add(histxy);
 
         // Amp plot regarding channel only
-        if (channel!=0){
-            // float x_mean = histxy->GetMean(1);
-            // float x_std = histxy->GetStdDev(1);
-            // float y_mean = histxy->GetMean(2);
-            // float y_std = histxy->GetStdDev(2);
-
-            // float ch_limits[4] = {x_mean-x_std, x_mean+x_std, y_mean-y_std, y_mean+y_std};
-
+        if (channel!=0 && channel!=7){
+            TCut ch_cut = Form("x_dut[0]>%f && x_dut[0]<%f && y_dut[0]>%f && y_dut[0]<%f",ch_limits[channel][0],
+                            ch_limits[channel][1],ch_limits[channel][2],ch_limits[channel][3]);
             auto htitleamp_chcut = Form("amp[%i] only channel;amp[%i] [mV];Counts",channel,channel);
             TH1F *histamp_chcut = new TH1F(Form("amp%i_chcut",channel),htitleamp_chcut,400,0,400);
             histamp_chcut->SetLineColor(kRed);
-            TCut ch_cut = Form("x_dut[0]>%f && x_dut[0]<%f && y_dut[0]>%f && y_dut[0]<%f",ch_limits[channel][0],
-                            ch_limits[channel][1],ch_limits[channel][2],ch_limits[channel][3]);
             chain->Draw(Form("amp[%i]>>amp%i_chcut",channel,channel),Form("amp[%i]>0",channel)+ch_cut);
+
+            hamp_Tot_chcut->Add(histamp_chcut);
         }
 
         // Time resolution
-        auto htitle_tRes = Form("#Delta t = time[7] - time[%i];#Delta t [s];Counts",channel);
-        TH1F *hist_tRes = new TH1F(Form("tRes%i",channel),htitle_tRes,100,0,0);
-        chain->Draw(Form("LP2_20[7]-LP2_20[%i]>>tRes%i",channel,channel),
-                    Form("LP2_20[%i]!=0 && LP2_20[7]!=0",channel));
-        TH1F *hist_tRes2 = new TH1F(Form("tRes%i2",channel),htitle_tRes,100,9.5e-9,11.5e-9);
-        chain->Draw(Form("LP2_20[7]-LP2_20[%i]>>tRes%i2",channel,channel),
-                    Form("LP2_20[%i]!=0 && LP2_20[7]!=0",channel));
+        if (channel!=7){
+            auto htitle_tRes = Form("#Delta t = time[7] - time[%i];#Delta t [s];Counts",channel);
+            TH1F *hist_tRes = new TH1F(Form("tRes%i",channel),htitle_tRes,100,0,0);
+            chain->Draw(Form("LP2_20[7]-LP2_20[%i]>>tRes%i",channel,channel),
+                        Form("LP2_20[%i]!=0 && LP2_20[7]!=0",channel));
+            TH1F *hist_tRes2 = new TH1F(Form("tRes%i2",channel),htitle_tRes,100,9.5e-9,11.5e-9);
+            chain->Draw(Form("LP2_20[7]-LP2_20[%i]>>tRes%i2",channel,channel),
+                        Form("LP2_20[%i]!=0 && LP2_20[7]!=0",channel));
+        }
     }
+
+    hamp_Tot_chcut->SetLineColor(kRed);
 
     // Close and save!
     output->Write();
